658fa1db49c5cca305b2acf8c5cf35b005385834
jamsrahimi <rahimi@protonmail.com>
2018-01-25 21:42:52 +0100

    2nd push

--- week2/filter-map.js
+++ week2/filter-map.js
@@ -64,9 +64,13 @@ let tasks = monday.concat(tuesday);
 //2. Write a program that does the following below.Use map and filter.You will also need a forEach
 //or a for loop for the 'summing up' part.
 // Collect two days' worth of tasks.
-tuesday.forEach(function (el, idx, arr) {
+tasks.forEach(function (el, idx, arr) {
     console.log(el);
-})
+});
+const dur = tasks.name.map(function (person) {
+    console.log(person.name);
+
+});
 // Convert the task durations to hours, instead of minutes.
 // Filter out everything that took two hours or more(remove from the collection)
 // Multiply the each duration by a per - hour rate for billing(you can decide yourself what
