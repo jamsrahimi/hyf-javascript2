8f025348a274005d46b7108edf0d1e96a6a2c880
jamsrahimi <rahimi@protonmail.com>
2018-02-01 05:30:16 +0100

    1st commit of js2-week3

--- week3/hw-closure.js
+++ week3/hw-closure.js
@@ -1,19 +1,20 @@
 'use strict'
 //1.We saw that we can pass functions as arguments to other functions.
 //Your task is to write a function that takes another function as an argument and runs it.
-
+//http://conceptf1.blogspot.nl/2013/11/javascript-closures.html
 function foo(func) {
     let doing = "What to do here?";
-    let done=
+    let done = function () {
+        console.log(doing);
+        func();
+        console.log("I am calling another function from inside this function. Also practicing Clusure!");
+    };
+    done();
 }
 
 function bar() {
     console.log('Hello, I am bar!');
 }
 foo(bar);
-//http://conceptf1.blogspot.nl/2013/11/javascript-closures.html
-function sayHelloToClosures(yourName) {
-    var text = 'Hello Closures from' + yourName;
-    var sayAlert = function () { alert(text); }
-    sayAlert();
-}
+console.log("-------------------------0");
+
